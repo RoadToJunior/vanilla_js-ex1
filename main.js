afb6288cd6aa7c289f744a0f8ffe4d926d4c65e5
NowWeAre-Free <kontodarm@wp.pl>
2023-08-09 15:45:12 +0200

    :tada: init

---
+++ main.js
@@ -0,0 +1,9 @@
+//1. Napisz funkcję która zwraca sumę wszystkich liczb zawartych w tablicy z wykorzsytaniem metody map.
+//1b. Napisz drugą wersję tej funkcji z wykorzystaniem pętli for of.
+
+//2. Napisz funkcję która zwraca sume wartości z przedziału dwóch liczb wprowadzonych do funkcji.
+
+const arr = [4, 3, 645];
+addNumbersFromArray(arr);
+
+function addScope(a, b) {}
