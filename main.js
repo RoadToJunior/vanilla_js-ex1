0ff794f9451e04a874ffc9fd480e56c01e9d9f6c
NowWeAre-Free <kontodarm@wp.pl>
2023-08-09 20:10:43 +0200

    for of

--- main.js
+++ main.js
@@ -3,7 +3,19 @@
 
 //2. Napisz funkcję która zwraca sume wartości z przedziału dwóch liczb wprowadzonych do funkcji.
 
+let sum = 0;
+
+const addNumbersFromArray = (array) => {
+  for (n of array) {
+    n += sum;
+  }
+};
+
+const addNumbersFromArray = (array) => {
+  return array.map((n) => (n += sum));
+};
+
 const arr = [4, 3, 645];
 addNumbersFromArray(arr);
 
-function addScope(a, b) {}
+// function addScope(a, b) {}
